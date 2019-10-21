4dea97faec4ba2f27274d56e23d423b72fddd21f
wangxiao <wangxiao@ybm100.com>
2019-10-21 20:51:43 +0800

    提交项目初始化

---
+++ src/test/java/com/wx/control/controlscheduled/ControlScheduledApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wx.control.controlscheduled;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlScheduledApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
